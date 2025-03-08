90bb6b3d38019dab78e40725de1601b23c64d5de
Dasha <darabrueva65@gmail.com>
2025-03-08 11:27:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleSumOfDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSumOfDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число");
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine(SumOfDigits(num));
+        }
+        static int SumOfDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+
+    }
+}
